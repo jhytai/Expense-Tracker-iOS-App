1208d918be9781fe122bfacbbed2f5eafd57aacb
Danyal Nemati <Dnemati@csu.fullerton.edu>
2023-11-16 23:09:31 -0800

    added two tab views

---
+++ Expense Tracker/Expense Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expense Tracker
+//
+//  Created by Danyal Nemati on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
