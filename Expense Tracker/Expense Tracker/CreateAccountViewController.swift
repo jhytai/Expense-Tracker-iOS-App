545ba73f6cca7c6fe6d1236967909ea2cd8767ad
Danyal Nemati <Dnemati@csu.fullerton.edu>
2023-11-22 01:23:10 -0800

    added the signup and login files connecting the ui buttons and the actions to the files

---
+++ Expense Tracker/Expense Tracker/CreateAccountViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CreateAccountViewController.swift
+//  Expense Tracker
+//
+//  Created by Danyal Nemati on 11/22/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func signupClicked(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
