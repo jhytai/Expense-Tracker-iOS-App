9f81daed170b592be3dd34963cb70c2bdfb1643e
Maria Fernandez <mariaibis@csu.fullerton.edu>
2023-11-25 14:05:56 +0100

    Created TrackExpensesController and added view controller for it.

---
+++ Expense Tracker/Expense Tracker/TrackExpensesController.swift
@@ -0,0 +1,13 @@
+//
+//  TrackExpensesController.swift
+//  Expense Tracker
+//
+//  Created by Maria Fernandez on 11/25/23.
+//
+
+import UIKit
+import Firebase
+
+class TrackExpensesController: UIViewController {
+    
+}
