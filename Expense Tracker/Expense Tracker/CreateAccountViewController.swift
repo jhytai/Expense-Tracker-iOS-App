633924db9c6694ad50899bdc8db8a987be449ede
jhytai <josue.tai@csu.fullerton.edu>
2023-11-26 02:19:47 -0800

    Included Constraints

--- Expense Tracker/Expense Tracker/CreateAccountViewController.swift
+++ Expense Tracker/Expense Tracker/CreateAccountViewController.swift
@@ -8,40 +8,43 @@
 import UIKit
 import Firebase
 
-class CreateAccountViewController: UIViewController {
+class CreateAccountViewController: UIViewController
+{
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
 
-    @IBAction func signupClicked(_ sender: UIButton) {
+    @IBAction func signupClicked(_ sender: UIButton)
+    {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
-            if let e = error {
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult,
+            error in if let e = error {
                 print("Error creating user: \(e.localizedDescription)")
             } else {
                 self.performSegue(withIdentifier: "goToNext", sender: self)
             }
         }
     }
-    
-    func setBackGround(){
+
+    func setBackGround()
+    {
         let colorTop = UIColor(red: 132.0/255.0, green: 104.0/255.0, blue: 212.0/255.0, alpha: 1.0).cgColor
         let colorBottom = UIColor(red: 39.0/255.0, green: 16.0/255.0, blue: 107.0/255.0, alpha: 1.0).cgColor
-        
+
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop, colorBottom]
         gradientLayer.locations = [0.0, 1.0]
         gradientLayer.frame = self.view.bounds
     }
-    
+
     /*
     // MARK: - Navigation
 
--- Expense Tracker/Expense Tracker/LoginViewController.swift
+++ Expense Tracker/Expense Tracker/LoginViewController.swift
@@ -8,29 +8,32 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController
+{
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func loginClicked(_ sender: UIButton) {
+    @IBAction func loginClicked(_ sender: UIButton)
+    {
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         
-        Auth.auth().signIn(withEmail: email, password: password) { firebaseResult, error in
-            if let e = error {
+        Auth.auth().signIn(withEmail: email, password: password) { firebaseResult,
+            error in if let e = error {
                 print("error")
             } else {
                 self.performSegue(withIdentifier: "goToNext", sender: self)
             }
         }
     }
-    
+
     /*
     // MARK: - Navigation
 
