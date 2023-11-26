74f8bcdfb3a8b7a65745aa875e3382c767a2c370
jhytai <josue.tai@csu.fullerton.edu>
2023-11-26 02:37:59 -0800

    Included pages for expenses and history

---
+++ Expense Tracker/Expense Tracker/Expenses.swift
@@ -0,0 +1,19 @@
+//
+//  Expenses.swift
+//  Expense Tracker
+//
+//  Created by Josh Tai on 11/26/23.
+//
+
+import UIKit
+
+class Expenses: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Expense Tracker/Expense Tracker/History.swift
@@ -0,0 +1,19 @@
+//
+//  History.swift
+//  Expense Tracker
+//
+//  Created by Josh Tai on 11/26/23.
+//
+
+import UIKit
+
+class History: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
