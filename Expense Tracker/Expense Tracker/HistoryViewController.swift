03810a67544bb231ba36c9f181485ac4263378cb
jhytai <josue.tai@csu.fullerton.edu>
2023-11-26 23:22:49 -0800

    Expenses and History pages

--- Expense Tracker/Expense Tracker/HistoryViewController.swift
+++ Expense Tracker/Expense Tracker/HistoryViewController.swift
@@ -7,16 +7,37 @@
 
 import UIKit
 
-class HistoryViewController: UIViewController
-{
+class HistoryViewController: UIViewController {
 
+    private var histPageTitle     : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-//    override func loadView() {
-//        // Code here
-//    }
+    override func loadView() {
+        super.loadView()
+        
+        // Initialize Expenses Page Title Label
+        self.histPageTitle = UILabel()
+        self.histPageTitle.text = "Expenses History"
+        self.histPageTitle.font = UIFont.systemFont(ofSize: 32)
+        self.histPageTitle.numberOfLines = 0
+        self.histPageTitle.lineBreakMode = .byWordWrapping
+        self.histPageTitle.textAlignment = .center
+        self.histPageTitle.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(histPageTitle)
+        
+        // Set up contraints
+        NSLayoutConstraint.activate([
+            // Constrain welcomeLabel
+            self.histPageTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            self.histPageTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            self.histPageTitle.leadingAnchor.constraint(greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            self.histPageTitle.trailingAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            ])
+    }
 
 }
