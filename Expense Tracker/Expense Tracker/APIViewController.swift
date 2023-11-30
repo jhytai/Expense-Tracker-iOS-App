7c5ada988c04de8f083b35477296289e1236f885
Indrajeet Patwardhan <Indrajeet2002@csu.fullerton.edu>
2023-11-29 19:19:19 -0800

    Set up functions and variables to use api

--- Expense Tracker/Expense Tracker/APIViewController.swift
+++ Expense Tracker/Expense Tracker/APIViewController.swift
@@ -7,7 +7,51 @@
 
 import UIKit
 
+enum Endpoint: String {
+    case get = ""
+}
+
 class APIViewController: UIViewController {
+    
+    private static let baseURLString = ""
+    
+//    static var getDogsURL: URL{
+//        return self.getDogsURL(
+//            endpoint: Endpoint.get,
+//            parameters: nil
+//        )
+//    }
+    
+    private static func dogURL(
+        endpoint: Endpoint,
+        parameters: [String:String]?
+    )->URL
+    {
+        let endpoint = self.baseURLString + endpoint.rawValue
+        
+        var components = URLComponents(string: endpoint)!
+        
+        var queryItems = [URLQueryItem]()
+        
+        let baseQueryParameters = [
+            "dummy": "value"
+        ]
+        for(key,value) in baseQueryParameters{
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParameters = parameters{
+            for(key, value) in additionalParameters{
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        
+        components.queryItems = queryItems
+        
+        return components.url!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
