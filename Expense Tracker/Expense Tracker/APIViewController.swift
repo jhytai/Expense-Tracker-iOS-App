a3f0b237596892d979a8a174f7b4dcc8194d5739
jhytai <josue.tai@csu.fullerton.edu>
2023-11-29 18:07:25 -0800

    Created API View

---
+++ Expense Tracker/Expense Tracker/APIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  APIViewController.swift
+//  Expense Tracker
+//
+//  Created by Josh Tai on 11/29/23.
+//
+
+import UIKit
+
+class APIViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func loadView() {
+        super.loadView()
+    }
+    
+    
+    
+}
